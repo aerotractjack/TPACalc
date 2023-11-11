fe4dde2020baffa1e2c5d791e9b01933b30520c6
jack <jackw@aerotractone.com>
2023-11-11 14:54:01 -0800

    work

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /home/aerotract/software/TPACalc
+pushd /home/aerotract/software/TPA/TPACalc
 rm -rf *.egg-info/ dist/ build/
 /usr/bin/python3 setup.py sdist bdist_wheel
 /usr/bin/python3 -m pip install --upgrade --force-reinstall dist/tpacalc-0.1-py3-none-any.whl
