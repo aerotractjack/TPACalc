f575a31bd3b7c99b6f75e3e2cff65744683b8f53
jack <jackw@aerotractone.com>
2023-11-11 13:35:09 -0800

    work

--- TPAcalc/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tpacalc import TPACalc
--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 pushd /home/aerotract/software/TPACalc
 rm -rf *.egg-info/ dist/ build/
 /usr/bin/python3 setup.py sdist bdist_wheel
-pip install --upgrade --force-reinstall dist/tpacalc-0.1-py3-none-any.whl
-echo "done" >> test.txt
+/usr/bin/python3 -m pip install --upgrade --force-reinstall dist/tpacalc-0.1-py3-none-any.whl
 popd
